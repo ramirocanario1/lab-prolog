0c6f34c83f87ef5be51a797febd48358d16adb17
= <ramirocanario1@gmail.com>
2022-04-09 21:04:47 -0300

    agregué los empleados y sus cualidades a la base de conocimiento

--- main.pl
+++ main.pl
@@ -1,1 +1,12 @@
-# Archivo principal
+tieneCualidad(carlos,   ["buena atencion", "logistica", "fuerza"]).
+tieneCualidad(marisa,   ["buena atencion", "fuerza"]).
+tieneCualidad(juan,     ["movilidad", "disponibilidad horaria"]).
+tieneCualidad(jimena,   ["logistica", "disponibilidad horaria"]).
+tieneCualidad(hector,   ["logistica", "buena atencion", "fuerza"]).
+tieneCualidad(betty,    ["movilidad", "buena atencion", "disponibilidad horaria"]).
+tieneCualidad(lucia,    ["buena atencion"]).
+tieneCualidad(axel,     ["fuerza", "movilidad"]).
+tieneCualidad(eva,      ["movilidad", "logistica"]).
+tieneCualidad(miguel,   ["buena atencion", "disponibilidad horaria"]).
+tieneCualidad(clara,    ["fuerza"]).
+tieneCualidad(luis,     ["logistica"]).
